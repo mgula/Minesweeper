756c04fed5975002163b2e242212f64b68ccc500
Marcus Gula <mgula@udel.edu>
2017-09-13 16:27:41 +0000

    mid way

--- main.c
+++ main.c
@@ -5,40 +5,61 @@
 #include "tile.h"
 #include "cursor.h"
 
+//to do: sudden death
+
+#define EASY .05
+#define INTERMEDIATE .10
+#define HARD .20
+
 #define SMALL 7
 #define MEDIUM 9
 #define LARGE 11
 
-typedef enum {NONE, MOVE_UP, MOVE_DOWN, MOVE_RIGHT, MOVE_LEFT, MARK_MINE, ACTIVATE, QUIT} Action;
+typedef enum {NONE, MOVE_UP, MOVE_DOWN, MOVE_RIGHT, MOVE_LEFT, MARK_MINE, ACTIVATE, PRINT, QUIT} Action;
 
 int length;
 int width;
 
-bool play = true;
+double difficulty;
 
-char currentCommand;
+bool play = true;
+bool firstMove = true;
+
+char currentCommand = 'h';
 Action currentAction = NONE;
+
 Cursor* cursor;
+
+char* getDifficultyString() {
+    if (difficulty == EASY) {
+        return "easy";
+    } else if (difficulty == INTERMEDIATE) {
+        return "intermediate";
+    } else if (difficulty == HARD) {
+        return "hard";
+    }
+}
 
 void printControls() {
     printf("w = move cursor up\n");
-    printf("a = move cursor right\n");
+    printf("a = move cursor left\n");
     printf("s = move cursor down\n");
-    printf("d = move cursor left\n");
+    printf("d = move cursor right\n");
     printf("q = quit\n");
-    printf("space = activate at current cursor location\n");
+    printf("m = mark current tile as mine\n");
+    printf("n = uncover current tile\n");
 }
 
 void readInput() {
     switch (currentCommand) {
         case 'a':
-            currentAction = MOVE_RIGHT;
+            currentAction = MOVE_LEFT;
             break;
         case 's':
             currentAction = MOVE_DOWN;
             break;
         case 'd':
-            currentAction = MOVE_LEFT;
+            currentAction = MOVE_RIGHT;
             break;
         case 'w':
             currentAction = MOVE_UP;
@@ -50,7 +71,7 @@ void readInput() {
             currentAction = MARK_MINE;
             break;
         case 'h':
-            printControls();
+            currentAction = PRINT;
             break;
         case 'n':
             currentAction = ACTIVATE;
@@ -61,26 +82,30 @@ void readInput() {
     }
 }
 
-void evaluateInput(Cursor* c) {
+void evaluateInput(Tile* board[][width]) {
     switch (currentAction) {
-        case NONE:
-            break;
-        case MOVE_UP:
-            moveCursor(c, 0, -1, width, length);
+        case MOVE_LEFT:
+            moveCursor(cursor, -1, 0, width, length);
             break;
         case MOVE_DOWN:
-            moveCursor(c, 0, 1, width, length);
-            break;
-        case MOVE_LEFT:
-            moveCursor(c, 1, 0, width, length);
+            moveCursor(cursor, 0, 1, width, length);
             break;
         case MOVE_RIGHT:
-            moveCursor(c, -1, 0, width, length);
+            moveCursor(cursor, 1, 0, width, length);
+            break;
+        case MOVE_UP:
+            moveCursor(cursor, 0, -1, width, length);
             break;
         case QUIT:
             play = false;
             break;
         case MARK_MINE:
+            markMine(board);
+            break;
+        case PRINT:
+            printControls();
+            break;
+        case ACTIVATE:
             break;
         default:
             break;
@@ -88,11 +113,12 @@ void evaluateInput(Cursor* c) {
     currentAction = NONE;
 }
 
+/*Methods for operations on the board.*/
 void printBoard(Tile* board[][width]) {
     for (int i = 0; i < length; i++) {
         for (int j = 0; j < width; j++) {
             if (i == cursor->y && j == cursor->x) {
-                printf("⇲ ");
+                printCursor(cursor, board[i][j]->markedAsMine, board[i][j]->surroundingMines);
             } else {
                 printTile(board[i][j]);
             }
@@ -101,23 +127,45 @@ void printBoard(Tile* board[][width]) {
     }
 }
 
+void markMine(Tile* board[][width]) {
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < width; j++) {
+            if (i == cursor->y && j == cursor->x) {
+                board[i][j]->markedAsMine = !board[i][j]->markedAsMine;
+            }
+        }
+    }
+}
+
 int main (int argc, char* argv[]) {
-    /*Set length and width of board.*/
-    if (argc == 1) {
+    /*Set difficulty, and length and width of board.*/
+    if (argc != 3) {
         /*For now, default to medium.*/
         length = width = MEDIUM;
-    } else if (argc == 2) {
+        difficulty = INTERMEDIATE;
+    } else if (argc == 3) {
         if (*argv[1] == 's' || *argv[1] == 'S') {
-            printf("Initializing board with size = small (4x4).\n");
             length = width = SMALL;
         } else if (*argv[1] == 'm' || *argv[1] == 'M') {
-            printf("Initializing board with size = medium (6x6).\n");
             length = width = MEDIUM;
         } else if (*argv[1] == 'l' || *argv[1] == 'L') {
-            printf("Initializing board with size = large (8x8).\n");
             length = width = LARGE;
+        } else {
+            /*default to medium if bad entry*/
+            length = width = MEDIUM;
+        }
+        if (*argv[2] == 'e' || *argv[2] == 'E') {
+            difficulty = EASY;
+        } else if (*argv[2] == 'i' || *argv[2] == 'I') {
+            difficulty = INTERMEDIATE;
+        } else if (*argv[2] == 'h' || *argv[2] == 'H') {
+            difficulty = HARD;
+        } else {
+            /*default to intermediate if bad entry*/
+            difficulty = INTERMEDIATE;
         }
     } 
+    printf("Initializing board with length = %d, width = %d, and difficulty = %s.\n", length, width, getDifficultyString());
     
     /*Initialize board with the set length and width, initially no mines.*/
     Tile* board[length][width];
@@ -138,7 +186,7 @@ int main (int argc, char* argv[]) {
         /*Prompt command.*/
         if (scanf(" %c", &currentCommand) != -1) {
             readInput();
-            evaluateInput(cursor);
+            evaluateInput(board);
         } else {
             printf("Invalid command.\n");
         }
